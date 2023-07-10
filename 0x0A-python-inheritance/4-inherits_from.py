175786610c3418ba9c40b70a6345c4906282a23e
catherinekimani <catherinekimani882@gmail.com>
2023-07-11 01:08:54 +0300

    Implement inherits_from function

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+""" """
+
+
+def inherits_from(obj, a_class):
+    """
+    Check if an object is an instance of a class that inherited
+    (directly or indirectly) from the specified class
+
+    Args:
+        obj: the object
+        class: class to compare
+    """
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
