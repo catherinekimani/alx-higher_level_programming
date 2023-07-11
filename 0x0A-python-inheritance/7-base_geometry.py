c6e24bba6fbb2428d0dea073b4f0ea21e823c1b1
catherinekimani <catherinekimani882@gmail.com>
2023-07-11 03:25:26 +0300

    Check pycode

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -19,7 +19,7 @@ class BaseGeometry:
             name (str): name of the value being validated
             value (int): value being validated
         """
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
