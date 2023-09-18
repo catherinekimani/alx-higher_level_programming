c38a4c0b39d39181900197330e5f5ffafe248553
catherinekimani <catherinekimani882@gmail.com>
2023-09-18 14:37:23 +0300

    Creates script that filters & displays states by name in the states table

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+script that takes in arguments and displays all values in the
+states table of hbtn_0e_0_usa where name matches the argument.
+"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+
+    # conncet db
+    my_db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                            password=argv[2], db=argv[3])
+    # create cursor object
+    my_cursor = my_db.cursor()
+
+    # execute SELECT query
+    my_cursor.execute("SELECT * FROM states WHERE BINARY name = %s", [argv[4]])
+
+    # fetch all
+    my_data = my_cursor.fetchall()
+
+    # iterate and print
+    for rows in my_data:
+        print(rows)
+
+    # close all cursors
+    my_cursor.close()
+
+    # close all bds
+    my_db.close()
