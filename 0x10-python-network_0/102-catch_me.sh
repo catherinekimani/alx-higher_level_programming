56add951afb6233fb67c384842e9119d20ca8220
catherinekimani <catherinekimani882@gmail.com>
2023-09-29 05:40:56 +0300

    Rewrite script

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that makes a request that causes the server to respond given message
-curl -o /dev/null -sw "You got me!" 0.0.0.0:5000/catch_me
+curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
