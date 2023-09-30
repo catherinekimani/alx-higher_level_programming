19ee6c6ead0606ff91c6ca2a8b8c5b5a6a99a17f
catherinekimani <catherinekimani882@gmail.com>
2023-09-30 04:51:30 +0300

    script that fetches & displays URL Response body with HTTP Status Code Handling

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+script that takes in a URL
+sends a request to the URL
+and displays the body of the response (decoded in utf-8).
+"""
+
+
+from urllib import request
+from urllib import error
+from sys import argv
+
+
+if __name__ == "__main__":
+    try:
+        with request.urlopen(argv[1]) as response:
+            print(response.read().decode('utf-8'))
+    except error.HTTPError as e:
+        print("Error code: {}".format(e.code))
